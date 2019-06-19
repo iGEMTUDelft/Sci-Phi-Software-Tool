90aab358853879c96ae424cd5ecc64a094fff22f
Dennis Kenbeek <djmkenbeek97@gmail.com>
2019-06-19 23:19:17 +0200

    Added ranking of codons

--- codon_code.m
+++ codon_code.m
@@ -1,29 +1,71 @@
 clc, clear all
-input = 'atgtctaccaccatgggaattcaacctcctaaaaagaaacgtaaagttaatactattaatattgctaaaaatgacttctcagatattgaattagcagccattccatttaatacattagcagatcactatggtgaacgtttagcacgtgaacagttagcattagaacatgaatcatatgaaatgggtgaagcacgttttcgtaagatgttcgagcgtcagttaaaagcaggtgaagttgcagataatgcagcagccaaacctttaattactacattattacctaaaatgattgctcgtattaacgattggtttgaagaggttaaagcaaagcgtggtaaacgtcctacagcatttcagttcttacaagaaatcaaacctgaagcagttgcatatattactattaaaacaacattagcatgtttaacatcagcagataatacaacagttcaagcagttgcatcagcaattggtcgtgcaattgaagatgaagcacgttttggtcgtattcgtgatttagaagccaaacattttaaaaaaaatgttgaagaacagttaaacaaacgtgttggtcatgtttataaaaaagcatttatgcaggttgttgaagcagatatgttatcaaaaggtttattaggtggtgaagcatggtcatcatggcataaagaagattcaattcatgttggtgttcgttgtattgaaatgttaattgaatcaacaggtatggtttcattacatcgtcagaatgcaggtgttgttggtcaagattcagaaacaattgaattagcacctgaatatgcagaagcaattgcaacacgtgcaggtgcattagcaggtatttcaccaatgtttcagccttgtgttgttcctcctaaaccttggacaggtattacaggtggtggttattgggcaaatggtcgtcgtcctttagcattagttcgtacacattcaaaaaaagcattaatgcgttatgaagatgtttacatgcctgaagtttataaagccattaatattgcacagaatactgcatggaaaatcaacaagaaagttttagcagttgcaaatgttattactaaatggaaacattgtcctgttgaagatattcctgcaattgaacgtgaagaattaccaatgaaacctgaagatattgatatgaatcctgaagcattaacagcatggaaacgtgcagcagcagccgtttatcgtaaagataaagcacgtaaatcacgtcgtatttcattagagttcatgttagagcaagcaaacaagttcgcaaatcataaagccatttggtttccttataatatggattggcgtggtcgtgtttatgcagtttcaatgtttaatcctcaaggtaatgatatgaccaaaggtttattaaccttagctaaaggtaaacctattggtaaagaaggttattattggttaaaaatccatggtgcaaattgtgcaggtgttgataaagttccttttccagaacgtattaagttcattgaagaaaatcatgaaaatattatggcatgtgctaaatcaccattagaaaatacatggtgggcagaacaagattcacctttttgttttttagccttttgttttgaatatgcaggtgttcaacatcatggtttatcatataattgttcattaccattagcatttgatggttcatgttcaggtattcagcatttttcagcaatgttacgtgatgaagttggtggtcgtgcagttaacttattaccttcagaaacagttcaagatatctatggtattgttgctaaaaaagttaatgaaatcttacaggcagatgccattaatggtacagataatgaagttgttacagttacagatgaaaatacaggtgaaatatcagaaaaagttaaattaggtaccaaagcattagcaggtcaatggttagcatatggtgttacacgttcagttactaagcgttcagttatgacattagcatatggttcaaaagagttcggttttcgtcaacaggttttagaagataccattcaacctgcaattgattcaggtaaaggtttaatgtttactcaacctaatcaagcagcaggttatatggccaaattaatatgggaatcagtttcagttacagttgttgcagcagttgaagcaatgaattggttaaaatcagcagccaaattattagcagcagaagttaaagataaaaaaacaggtgaaatcttacgtaagcgttgtgcagttcattgggttacacctgatggttttcctgtttggcaagaatataaaaaacctattcaaacacgtttaaacttaatgtttttaggtcagttccgtttacaacctactattaatacaaacaaagattcagaaattgatgcacataaacaagaatcaggtattgcacctaacttcgttcattcacaagatggttcacatttacgtaaaacagttgtttgggcacatgaaaaatatggtattgaatcatttgcattaattcacgattcatttggtaccattccagcagatgcagcaaacttattcaaagcagttcgtgaaacaatggttgatacatatgaatcatgtgatgttttagcagacttctatgatcagttcgcagatcagttacatgaatcacagttagataaaatgcctgcattacctgccaaaggtaacttaaacttacgtgatattttagaatcagacttcgcatttgcctaa';
+input = 'gagacctatcagtgatagaggatccaaccgagcccaatataggactcagggtgccaaaaaaatgtctaccaccatgggaattcaacctcctaaaaagaaacgtaaagttaatactattaatattgctaaaaatgacttctcagatattgaattagcagccattccatttaatacattagcagatcactatggtgaacgtttagcacgtgaacagttagcattagaacatgaatcatatgaaatgggtgaagcacgttttcgtaagatgttcgagcgtcagttaaaagcaggtgaagttgcagataatgcagcagccaaacctttaattactacattattacctaaaatgattgctcgtattaacgattggtttgaagaggttaaagcaaagcgtggtaaacgtcctacagcatttcagttcttacaagaaatcaaacctgaagcagttgcatatattactattaaaacaacattagcatgtttaacatcagcagataatacaacagttcaagcagttgcatcagcaattggtcgtgcaattgaagatgaagcacgttttggtcgtattcgtgatttagaagccaaacattttaaaaaaaatgttgaagaacagttaaacaaacgtgttggtcatgtttataaaaaagcatttatgcaggttgttgaagcagatatgttatcaaaaggtttattaggtggtgaagcatggtcatcatggcataaagaagattcaattcatgttggtgttcgttgtattgaaatgttaattgaatcaacaggtatggtttcattacatcgtcagaatgcaggtgttgttggtcaagattcagaaacaattgaattagcacctgaatatgcagaagcaattgcaacacgtgcaggtgcattagcaggtatttcaccaatgtttcagccttgtgttgttcctcctaaaccttggacaggtattacaggtggtggttattgggcaaatggtcgtcgtcctttagcattagttcgtacacattcaaaaaaagcattaatgcgttatgaagatgtttacatgcctgaagtttataaagccattaatattgcacagaatactgcatggaaaatcaacaagaaagttttagcagttgcaaatgttattactaaatggaaacattgtcctgttgaagatattcctgcaattgaacgtgaagaattaccaatgaaacctgaagatattgatatgaatcctgaagcattaacagcatggaaacgtgcagcagcagccgtttatcgtaaagataaagcacgtaaatcacgtcgtatttcattagagttcatgttagagcaagcaaacaagttcgcaaatcataaagccatttggtttccttataatatggattggcgtggtcgtgtttatgcagtttcaatgtttaatcctcaaggtaatgatatgaccaaaggtttattaaccttagctaaaggtaaacctattggtaaagaaggttattattggttaaaaatccatggtgcaaattgtgcaggtgttgataaagttccttttccagaacgtattaagttcattgaagaaaatcatgaaaatattatggcatgtgctaaatcaccattagaaaatacatggtgggcagaacaagattcacctttttgttttttagccttttgttttgaatatgcaggtgttcaacatcatggtttatcatataattgttcattaccattagcatttgatggttcatgttcaggtattcagcatttttcagcaatgttacgtgatgaagttggtggtcgtgcagttaacttattaccttcagaaacagttcaagatatctatggtattgttgctaaaaaagttaatgaaatcttacaggcagatgccattaatggtacagataatgaagttgttacagttacagatgaaaatacaggtgaaatatcagaaaaagttaaattaggtaccaaagcattagcaggtcaatggttagcatatggtgttacacgttcagttactaagcgttcagttatgacattagcatatggttcaaaagagttcggttttcgtcaacaggttttagaagataccattcaacctgcaattgattcaggtaaaggtttaatgtttactcaacctaatcaagcagcaggttatatggccaaattaatatgggaatcagtttcagttacagttgttgcagcagttgaagcaatgaattggttaaaatcagcagccaaattattagcagcagaagttaaagataaaaaaacaggtgaaatcttacgtaagcgttgtgcagttcattgggttacacctgatggttttcctgtttggcaagaatataaaaaacctattcaaacacgtttaaacttaatgtttttaggtcagttccgtttacaacctactattaatacaaacaaagattcagaaattgatgcacataaacaagaatcaggtattgcacctaacttcgttcattcacaagatggttcacatttacgtaaaacagttgtttgggcacatgaaaaatatggtattgaatcatttgcattaattcacgattcatttggtaccattccagcagatgcagcaaacttattcaaagcagttcgtgaaacaatggttgatacatatgaatcatgtgatgttttagcagacttctatgatcagttcgcagatcagttacatgaatcacagttagataaaatgcctgcattacctgccaaaggtaacttaaacttacgtgatattttagaatcagacttcgcatttgcctaataaggcgcgccccaggcatcaaataaaacgaaaggctcagtcgaaagactgggcctttcgttttatctgttgtttgtcggtgaacgctctc';
 
-output = transform_seq(input, "nt");
+startpos = 62;
 
-disp(upper(input));
-disp(output);
-
-function new_seq = transform_seq(seq, type)  
-    if type == "nt"
-        seq = nt2aa(seq);
-        disp(seq)
-    end
+%propInput = oligoprop(input)
+%propOutput = oligoprop(output)
+input_CDS = upper(input(startpos:end))
+output_CDS = transform_seq(input, startpos)
     
-    if ~ischar(seq)
-        seq = char(seq);
+
+function new_seq = transform_seq(input, startpos)
+    
+    %get CDS from full mRNA
+    CDS = input(startpos:end);
+    AA_CDS = nt2aa(CDS, 'AlternativeStartCodons', false);
+    
+    %process the data
+    [Data_raw, AA_ref, Codon_ref, AA_list, n_orgs] = process_data();
+    
+    %make a list of preferred codons
+    variance = get_variance(Data_raw, AA_ref, AA_list, n_orgs);
+    [ordered_codons, ordered_variances] = get_ordered_list(variance, AA_ref, AA_list, Codon_ref);
+    
+    %restructure AA_ref (because ordering codons gives different structure)
+    for i = 1:length(AA_ref)
+        AA_ref(i) = nt2aa(char(ordered_codons(i)), 'AlternativeStartCodons', false);
     end
+      
+    seq_codon = [];
+    for i = 1:length(AA_CDS)
+        aa = AA_CDS(i);
+        
+        %matlab sometimes uses END and sometimes uses * for stop codons,
+        %force it to use * at all times
+        if aa == 'end'
+            aa = '*';
+        end
+        
+        %get the ordered codon list for this aminoacid
+        aa_codons = ordered_codons(find(AA_ref == aa));
+        codon = aa_codons(1); %grab the first one
+        
+        seq_codon = [seq_codon codon];
+    end
+    new_seq = char(strjoin(seq_codon));
+    new_seq = new_seq(~isspace(new_seq));
+    
+    %verify that the new sequence has the same AA sequence
+    if nt2aa(new_seq, 'AlternativeStartCodons', false) == AA_CDS
+        disp("Optimized");
+    end
+end
 
+function [Data_raw, AA_ref, Codon_ref, AA_list, n_orgs] = process_data()
+    %import data
     [~,~,Data_raw]=xlsread([pwd '/data_proof.xlsx']);
-
+    
+    %make reference lists
     AA_ref = string(Data_raw(1,13:end));
     AA_ref(find(AA_ref == "stop")) = "*";
-    Triplet_ref = string(Data_raw(2,13:end));
+    Codon_ref = string(Data_raw(2,13:end));
     AA_list = ["phe" "leu" "ile" "met" "val" "tyr" "*" "his" "gln" "asn" "lys" "asp" "glu" "ser" "pro" "thr" "ala" "cys" "trp" "arg" "gly"];
     n_orgs = length(Data_raw(3:end,1));
+end
 
+function variance = get_variance(Data_raw, AA_ref, AA_list, n_orgs)
+        
     freqs = zeros(n_orgs, length(AA_ref));
 
     for i = 1:n_orgs
@@ -46,36 +88,31 @@ function new_seq = transform_seq(seq, type)
        var_aa = var(freq_aa);
        variance = [variance var_aa];
     end
-
-    seq_codon = [];
-    for i = 1:length(seq)
-        aa = lower(aminolookup(seq(i)));
-        %matlab sometimes uses END and sometimes uses * for stop codons,
-        %force it to use * at all times
-        if aa == 'end'
-            aa = '*';
-        end
-        min_variance = min(variance(find(AA_ref == aa)));
-        min_variance_pos = find(variance == min_variance);
-
-        %it might occur that the min value also occurs somewhere else in the
-        %variance array, we should take the one from the right AA
-        if length(min_variance_pos) ~= 1
-            for ii = 1:length(min_variance_pos)
-               if AA_ref(min_variance_pos(ii)) == aa
-                  codon = Triplet_ref(min_variance_pos(ii));
-               end
-            end
-        else 
-            codon = Triplet_ref(variance == min_variance);
-        end
-        seq_codon = [seq_codon codon];
-    end
-    new_seq = char(strjoin(seq_codon));
-    new_seq = new_seq(~isspace(new_seq));
-    %verify that the new sequence has the same AA sequence
-    if nt2aa(new_seq) == seq
-        disp("Optimized");
-    end
+    
 end
 
+function [ordered_codons, ordered_variances] = get_ordered_list(variance, AA_ref, AA_list, Codon_ref)
+    ordered_codons = [];
+    ordered_variances = [];
+
+    for i = 1:length(AA_list)
+        %get the positions of this aminoacid
+        aa = AA_list(i);
+        aa_pos = find(AA_ref == aa);
+        
+        %get the list of codons and the variances
+        aa_codons = Codon_ref(aa_pos);
+        aa_variance = variance(aa_pos);
+        
+        %sort the variances
+        sorted_list = sort(aa_variance);
+        
+        %use the ordered variance list to order the codons
+        for ii = 1:length(aa_codons)
+            pos = find(aa_variance  == sorted_list(ii));
+            codon = aa_codons(pos);
+            ordered_codons = [ordered_codons codon];
+        end
+        ordered_variances = [ordered_variances sorted_list];
+    end
+end
