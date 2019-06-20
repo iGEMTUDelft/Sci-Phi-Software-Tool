a18d69f8a0b217740c8d95e75ee39f541d16ca83
Dennis Kenbeek <djmkenbeek97@gmail.com>
2019-06-20 10:11:22 +0200

    Added restriction site elimination

--- codon_code.m
+++ codon_code.m
@@ -1,15 +1,14 @@
-clc, clear all
+
 input = 'gagacctatcagtgatagaggatccaaccgagcccaatataggactcagggtgccaaaaaaatgtctaccaccatgggaattcaacctcctaaaaagaaacgtaaagttaatactattaatattgctaaaaatgacttctcagatattgaattagcagccattccatttaatacattagcagatcactatggtgaacgtttagcacgtgaacagttagcattagaacatgaatcatatgaaatgggtgaagcacgttttcgtaagatgttcgagcgtcagttaaaagcaggtgaagttgcagataatgcagcagccaaacctttaattactacattattacctaaaatgattgctcgtattaacgattggtttgaagaggttaaagcaaagcgtggtaaacgtcctacagcatttcagttcttacaagaaatcaaacctgaagcagttgcatatattactattaaaacaacattagcatgtttaacatcagcagataatacaacagttcaagcagttgcatcagcaattggtcgtgcaattgaagatgaagcacgttttggtcgtattcgtgatttagaagccaaacattttaaaaaaaatgttgaagaacagttaaacaaacgtgttggtcatgtttataaaaaagcatttatgcaggttgttgaagcagatatgttatcaaaaggtttattaggtggtgaagcatggtcatcatggcataaagaagattcaattcatgttggtgttcgttgtattgaaatgttaattgaatcaacaggtatggtttcattacatcgtcagaatgcaggtgttgttggtcaagattcagaaacaattgaattagcacctgaatatgcagaagcaattgcaacacgtgcaggtgcattagcaggtatttcaccaatgtttcagccttgtgttgttcctcctaaaccttggacaggtattacaggtggtggttattgggcaaatggtcgtcgtcctttagcattagttcgtacacattcaaaaaaagcattaatgcgttatgaagatgtttacatgcctgaagtttataaagccattaatattgcacagaatactgcatggaaaatcaacaagaaagttttagcagttgcaaatgttattactaaatggaaacattgtcctgttgaagatattcctgcaattgaacgtgaagaattaccaatgaaacctgaagatattgatatgaatcctgaagcattaacagcatggaaacgtgcagcagcagccgtttatcgtaaagataaagcacgtaaatcacgtcgtatttcattagagttcatgttagagcaagcaaacaagttcgcaaatcataaagccatttggtttccttataatatggattggcgtggtcgtgtttatgcagtttcaatgtttaatcctcaaggtaatgatatgaccaaaggtttattaaccttagctaaaggtaaacctattggtaaagaaggttattattggttaaaaatccatggtgcaaattgtgcaggtgttgataaagttccttttccagaacgtattaagttcattgaagaaaatcatgaaaatattatggcatgtgctaaatcaccattagaaaatacatggtgggcagaacaagattcacctttttgttttttagccttttgttttgaatatgcaggtgttcaacatcatggtttatcatataattgttcattaccattagcatttgatggttcatgttcaggtattcagcatttttcagcaatgttacgtgatgaagttggtggtcgtgcagttaacttattaccttcagaaacagttcaagatatctatggtattgttgctaaaaaagttaatgaaatcttacaggcagatgccattaatggtacagataatgaagttgttacagttacagatgaaaatacaggtgaaatatcagaaaaagttaaattaggtaccaaagcattagcaggtcaatggttagcatatggtgttacacgttcagttactaagcgttcagttatgacattagcatatggttcaaaagagttcggttttcgtcaacaggttttagaagataccattcaacctgcaattgattcaggtaaaggtttaatgtttactcaacctaatcaagcagcaggttatatggccaaattaatatgggaatcagtttcagttacagttgttgcagcagttgaagcaatgaattggttaaaatcagcagccaaattattagcagcagaagttaaagataaaaaaacaggtgaaatcttacgtaagcgttgtgcagttcattgggttacacctgatggttttcctgtttggcaagaatataaaaaacctattcaaacacgtttaaacttaatgtttttaggtcagttccgtttacaacctactattaatacaaacaaagattcagaaattgatgcacataaacaagaatcaggtattgcacctaacttcgttcattcacaagatggttcacatttacgtaaaacagttgtttgggcacatgaaaaatatggtattgaatcatttgcattaattcacgattcatttggtaccattccagcagatgcagcaaacttattcaaagcagttcgtgaaacaatggttgatacatatgaatcatgtgatgttttagcagacttctatgatcagttcgcagatcagttacatgaatcacagttagataaaatgcctgcattacctgccaaaggtaacttaaacttacgtgatattttagaatcagacttcgcatttgcctaataaggcgcgccccaggcatcaaataaaacgaaaggctcagtcgaaagactgggcctttcgttttatctgttgtttgtcggtgaacgctctc';
 
 startpos = 62;
 
 %propInput = oligoprop(input)
 %propOutput = oligoprop(output)
-input_CDS = upper(input(startpos:end))
-output_CDS = transform_seq(input, startpos)
-    
+input_CDS = upper(input(startpos:end));
+output_CDS = transform_seq(input, startpos);
 
-function new_seq = transform_seq(input, startpos)
+function char_seq_codon = transform_seq(input, startpos)
     
     %get CDS from full mRNA
     CDS = input(startpos:end);
@@ -26,30 +25,29 @@ function new_seq = transform_seq(input, startpos)
     for i = 1:length(AA_ref)
         AA_ref(i) = nt2aa(char(ordered_codons(i)), 'AlternativeStartCodons', false);
     end
-      
+    
+    %make initial optimized sequence
     seq_codon = [];
     for i = 1:length(AA_CDS)
-        aa = AA_CDS(i);
+        aa_1 = AA_CDS(i);
         
         %matlab sometimes uses END and sometimes uses * for stop codons,
         %force it to use * at all times
-        if aa == 'end'
-            aa = '*';
+        if aa_1 == 'end'
+            aa_1 = '*';
         end
         
         %get the ordered codon list for this aminoacid
-        aa_codons = ordered_codons(find(AA_ref == aa));
-        codon = aa_codons(1); %grab the first one
+        aa_codons = ordered_codons(find(AA_ref == aa_1));
+        codon_1 = aa_codons(1); %grab the first one
         
-        seq_codon = [seq_codon codon];
+        seq_codon = [seq_codon codon_1];
     end
-    new_seq = char(strjoin(seq_codon));
-    new_seq = new_seq(~isspace(new_seq));
+    char_seq_codon = char(strjoin(seq_codon));
+    char_seq_codon = char_seq_codon(~isspace(char_seq_codon));
     
-    %verify that the new sequence has the same AA sequence
-    if nt2aa(new_seq, 'AlternativeStartCodons', false) == AA_CDS
-        disp("Optimized");
-    end
+    %now we will iterate over restriction sites and grab different codons
+    char_seq_codon = eliminate_restrictions(seq_codon, char_seq_codon, AA_ref, ordered_codons)
 end
 
 function [Data_raw, AA_ref, Codon_ref, AA_list, n_orgs] = process_data()
@@ -115,4 +113,98 @@ function [ordered_codons, ordered_variances] = get_ordered_list(variance, AA_ref
         end
         ordered_variances = [ordered_variances sorted_list];
     end
+end
+
+function output = eliminate_restrictions(seq_codon, char_seq_codon, AA_ref, ordered_codons)
+    %now we will iterate over restriction sites and grab different codons
+    empty = false;
+    while ~empty
+        enzyme_list = ["CfoI"]; %TODO import enzyme list
+        for i = 1:length(enzyme_list)
+            [~, sites] = rebasecuts(char_seq_codon, {char(enzyme_list(i))});
+            for ii = 1:length(sites)
+                choice = 2;        
+                fixed = false;
+                while ~fixed
+                    if mod(sites(ii), 3) == 0 %end of codon so change either this one or the next one
+                        codon = char(seq_codon(sites(i)/3));
+                        aa = nt2aa(codon, 'AlternativeStartCodons', false);
+                        alt_codons = ordered_codons(find(AA_ref == aa));  
+
+                        %change the codon for the next option
+                        seq_codon(sites(ii)/3) = alt_codons(choice);
+
+                        char_seq_codon = char(strjoin(seq_codon));
+                        char_seq_codon = char_seq_codon(~isspace(char_seq_codon));
+
+                        [~, sites_new] = rebasecuts(char_seq_codon, {char(enzyme_list(i))});
+                        check = find(sites_new == sites(ii));
+                        if isempty(check)
+                            fixed = true;
+                        else
+                            choice = choice + 1;
+                            if choice > length(alt_codons)
+                                fixed = true;
+                                disp('Couldnt find better codon')
+                            end
+                        end
+
+                    elseif mod(sites(ii), 3) == 2 %middle of codon so only change this one
+                        codon = char(seq_codon(sites(ii)/3 + 1/3));
+                        aa = nt2aa(codon, 'AlternativeStartCodons', false);
+                        alt_codons = ordered_codons(find(AA_ref == aa));  
+
+                        %change the codon for the next option
+                        seq_codon(sites(ii)/3 + 1/3) = alt_codons(choice);
+
+                        char_seq_codon = char(strjoin(seq_codon));
+                        char_seq_codon = char_seq_codon(~isspace(char_seq_codon));
+
+                        [~, sites_new] = rebasecuts(char_seq_codon, {char(enzyme_list(i))});
+                        check = find(sites_new == sites(ii));
+                        if isempty(check)
+                            fixed = true;
+                        else
+                            choice = choice + 1;
+                            if choice > length(alt_codons)
+                                fixed = true;
+                                disp('Couldnt find better codon')
+                            end                   
+                        end
+                    elseif mod(sites(ii), 3) == 1 %beginning of codon so either change this one or the previous one
+                        codon = char(seq_codon(sites(ii)/3 - 1/3));
+                        aa = nt2aa(codon, 'AlternativeStartCodons', false);
+                        alt_codons = ordered_codons(find(AA_ref == aa));  
+
+                        %change the codon for the next option
+                        seq_codon(sites(ii)/3 - 1/3) = alt_codons(choice);
+
+                        char_seq_codon = char(strjoin(seq_codon));
+                        char_seq_codon = char_seq_codon(~isspace(char_seq_codon));
+                        
+                        %check presence of the restriction site
+                        [~, sites_new] = rebasecuts(char_seq_codon, {char(enzyme_list(i))});
+                        check = find(sites_new == sites(ii));
+                        if isempty(check)
+                            fixed = true;
+                        else
+                            choice = choice + 1;
+                            if choice > length(alt_codons)
+                                fixed = true;
+                                disp('Couldnt find better codon')
+                            end
+                        end
+                    end
+                end
+            end
+        end
+        
+        %final check to see if nothing is left
+        [~, sites] = rebasecuts(char_seq_codon, {char(enzyme_list(i))});
+        disp(sites)
+        if isempty(sites)
+            empty = true;
+        end
+    end
+    output = char_seq_codon;
 end
